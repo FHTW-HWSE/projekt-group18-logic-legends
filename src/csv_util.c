b9896c0dfe905827bb9076313b84167efe120b98
Jakob Gridl <jakob@gridl.at>
2023-04-10 11:15:37 +0200

    gitignore, edit csv_util

--- src/csv_util.c
+++ src/csv_util.c
@@ -1,15 +1,31 @@
+#pragma once
+#include <stdio.h>
 
 /**
  * @brief write_csv takes two parameters, a filehandle and a data-string and appends the data at the end of the file in a new line.
- * 
- * @param file Filehandle of target csv-file to write to
- * @param data Data that should be added as a new line to the csv-file
- * @return int Returns the number of characters written successfully, -1 if an error occurred
+ *
+ * @param file Filehandle of target csv-file to write to.
+ * @param data Data that should be added as a new line to the csv-file.
+ * @return int Returns the number of characters written successfully, -1 if an error occurred.
  */
-int write_csv(void* file, char* data) {
+int write_csv(void *file, char *data)
+{
     int char_written = 0;
 
-    //TODO: write to file, count characters written in char_written
+    // TODO: write to file, count characters written in char_written
 
     return char_written;
+}
+
+/**
+ * @brief Prints the content of a given csv-file. The filehandle to the csv-file must be given in the parameter.
+ * 
+ * @param file Filehandle of the csv-file that should be printed.
+ * @return int Returns 0 if successfully printed, -1 if an error occurred.
+ */
+int print_csv(void *file)
+{
+    //TODO: print content to stdout
+
+    return 0;
 }
--- src/csv_util.h
+++ src/csv_util.h
@@ -1,2 +1,4 @@
-int write_csv(void* file, char* data);
+#pragma once
 
+int write_csv(void *file, char *data);
+int print_csv(void *file);
